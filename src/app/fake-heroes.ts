f7dd8f6982931cbd1981884ab3612018b31e88ce
massimo <massipetra@hotmail.it>
2019-04-09 22:45:42 +0200

    initial commit

---
+++ src/app/fake-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'star-lord', avatar: './assets/images/star-lord.jpg'},
+    {id: 2, name: 'drax', avatar: './assets/images/drax.jpg'},
+    {id: 3, name: 'gamora', avatar: './assets/images/gamora.jpg'},
+    {id: 4, name: 'groot', avatar: './assets/images/groot.jpg'},
+    {id: 5, name: 'rocket', avatar: './assets/images/rocket.jpg'},
+    {id: 6, name: 'yondu', avatar: './assets/images/yondu.jpg'},
+    {id: 7, name: 'nebula', avatar: './assets/images/nebula.jpg'},
+    {id: 8, name: 'ego', avatar: './assets/images/ego.jpg'},
+    {id: 9, name: 'mantis', avatar: './assets/images/mantis.jpg'},
+    {id: 10, name: 'ayesha', avatar: './assets/images/ayesha.jpg'}
+]
+
