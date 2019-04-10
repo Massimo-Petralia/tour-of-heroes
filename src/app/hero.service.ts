27174f2caf5fe657c1abdb5ee46ad088ec1122e9
massimo <massipetra@hotmail.it>
2019-04-10 11:10:25 +0200

    http and heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,18 +3,26 @@ import {Hero} from './hero';
 import {HEROES} from './fake-heroes';
 import {Observable, of} from 'rxjs';
 import {MessageService} from './message.service';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+    ) { }
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('MessageService: eroi recuperati');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: eroe recuperato id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+  private log(message: string){
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
