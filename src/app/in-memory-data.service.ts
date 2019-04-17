50c96339f05f7d8cfc93455b1276f766e03be462
massimo <massipetra@hotmail.it>
2019-04-17 09:25:30 +0200

    UPPERCASE NAMES

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -9,16 +9,16 @@ import { Hero } from './hero';
 export class InMemoryDataService {
 createDb() {
   const heroes = [
-    {id: 1, name: 'star-lord', avatar: './assets/images/star-lord.jpg'},
-    {id: 2, name: 'drax', avatar: './assets/images/drax.jpg'},
-    {id: 3, name: 'gamora', avatar: './assets/images/gamora.jpg'},
-    {id: 4, name: 'groot', avatar: './assets/images/groot.jpg'},
-    {id: 5, name: 'rocket', avatar: './assets/images/rocket.jpg'},
-    {id: 6, name: 'yondu', avatar: './assets/images/yondu.jpg'},
-    {id: 7, name: 'nebula', avatar: './assets/images/nebula.jpg'},
-    {id: 8, name: 'ego', avatar: './assets/images/ego.jpg'},
-    {id: 9, name: 'mantis', avatar: './assets/images/mantis.jpg'},
-    {id: 10, name: 'ayesha', avatar: './assets/images/ayesha.jpg'}
+    {id: 1, name: 'Star-lord', avatar: './assets/images/star-lord.jpg'},
+    {id: 2, name: 'Drax', avatar: './assets/images/drax.jpg'},
+    {id: 3, name: 'Gamora', avatar: './assets/images/gamora.jpg'},
+    {id: 4, name: 'Groot', avatar: './assets/images/groot.jpg'},
+    {id: 5, name: 'Rocket', avatar: './assets/images/rocket.jpg'},
+    {id: 6, name: 'Yondu', avatar: './assets/images/yondu.jpg'},
+    {id: 7, name: 'Nebula', avatar: './assets/images/nebula.jpg'},
+    {id: 8, name: 'Ego', avatar: './assets/images/ego.jpg'},
+    {id: 9, name: 'Mantis', avatar: './assets/images/mantis.jpg'},
+    {id: 10, name: 'Ayesha', avatar: './assets/images/ayesha.jpg'}
   ];
   return {heroes};
 }
